77c7f7aaa35c717f001b8a545b45689b5181b0a2
francisparrillarye <36092836+francisparrillarye@users.noreply.github.com>
2018-03-06 11:39:15 -0500

    Update test.php

--- php_test/test.php
+++ php_test/test.php
@@ -1,6 +1,6 @@
 <?php
-include "function.php"
 class PHPTest extends PHPUnit_Framework_TestCase{
+  include "function.php"
   $value=4; 
 public function test(){
   $this->assertEqual(exp($value),4);
